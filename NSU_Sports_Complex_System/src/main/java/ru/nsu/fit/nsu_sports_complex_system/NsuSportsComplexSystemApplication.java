bf342c585a177fda2f1c250d85ce00c24c0c682b
Olga <o.pogibelnaya@g.nsu.ru>
2024-10-09 00:44:59 +0700

    SCRUM-16 Hello World with Spring Boot

---
+++ NSU_Sports_Complex_System/src/main/java/ru/nsu/fit/nsu_sports_complex_system/NsuSportsComplexSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.nsu_sports_complex_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsuSportsComplexSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsuSportsComplexSystemApplication.class, args);
+	}
+
+}
