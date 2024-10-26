670515e8a34481da941b9f59a265d231c590a948
Olga <o.pogibelnaya@g.nsu.ru>
2024-10-26 13:13:21 +0700

    SCRUM-34: added Users tests
    SCRUM-34--users-tests
    
    SCRUM-34: Users tests
    
    add user tests
    
    SCRUM-34: Users tests
    
    delete unused import

--- backend/src/main/java/ru/nsu/sports/complex/backend/model/User.java
+++ backend/src/main/java/ru/nsu/sports/complex/backend/model/User.java
@@ -9,10 +9,19 @@ import lombok.Setter;
 @Entity
 @Table(name = "users")
 public class User {
+    public User() {}
+
+    public User(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
     private String email;
+
 }
