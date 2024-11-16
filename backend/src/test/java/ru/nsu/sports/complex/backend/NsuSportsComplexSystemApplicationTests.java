ff333cc2a39d2b97bfe8882fb81602750372e204
Nikolay <58514394+nocarend@users.noreply.github.com>
2024-11-16 11:24:40 +0700

    scrum-41: bug fix

--- backend/src/test/java/ru/nsu/sports/complex/backend/NsuSportsComplexSystemApplicationTests.java
+++ backend/src/test/java/ru/nsu/sports/complex/backend/NsuSportsComplexSystemApplicationTests.java
@@ -1,5 +1,6 @@
 package ru.nsu.sports.complex.backend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class NsuSportsComplexSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Hello world");
+		Assertions.assertTrue(true);
 	}
 
 }
