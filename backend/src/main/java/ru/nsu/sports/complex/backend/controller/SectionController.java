1a361075be53d11902b86213a69ad6c3e9cff959
Olga <o.pogibelnaya@g.nsu.ru>
2024-11-23 09:56:34 +0700

    scrum-35: при запросе секции с несуществующим id возвращается информативная ошибка

--- backend/src/main/java/ru/nsu/sports/complex/backend/controller/SectionController.java
+++ backend/src/main/java/ru/nsu/sports/complex/backend/controller/SectionController.java
@@ -37,11 +37,10 @@ public class SectionController {
     @GetMapping("/{id}")
     public ResponseEntity<SectionDTO> findById(@PathVariable Integer id) {
         Section section = service.findById(id);
-        if (section != null) {
-            return new ResponseEntity<>(converter.sectionToDTO(section), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (section == null) {
+            throw new NoSuchElementException("Section with ID " + id + " does not exist");
         }
+        return new ResponseEntity<>(converter.sectionToDTO(section), HttpStatus.OK);
     }
 
     @Operation(summary = "Получить информацию о секции по названию.")
