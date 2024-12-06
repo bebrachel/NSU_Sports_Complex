51f68018b9287035f4ce0cbde3f7bd126cf967e4
Olga <o.pogibelnaya@g.nsu.ru>
2024-12-06 22:19:55 +0700

    fixed tests after rebase

--- backend/src/main/java/ru/nsu/sports/complex/backend/service/SectionService.java
+++ backend/src/main/java/ru/nsu/sports/complex/backend/service/SectionService.java
@@ -1,5 +1,6 @@
 package ru.nsu.sports.complex.backend.service;
 
+import ru.nsu.sports.complex.backend.dto.SectionDTO;
 import ru.nsu.sports.complex.backend.model.Section;
 
 import java.util.List;
@@ -7,10 +8,16 @@ import java.util.List;
 
 public interface SectionService {
     Section findById(Integer id);
+
     Section findByName(String name);
+
     List<Section> findAllSections();
+
     Section createSection(Section section);
-    Section updateSection(Section section, Integer id);
+
+    Section updateSection(Integer id, SectionDTO sectionDTO);
+
     boolean deleteSectionById(Integer id);
+
     boolean deleteSectionByName(String name);
 }
