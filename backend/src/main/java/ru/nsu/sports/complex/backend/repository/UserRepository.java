fe91173efb07ab16d7447ed5897cd36a8525d237
Mikhail <misha_k_2018@mail.ru>
2024-12-13 21:45:23 +0700

    edits

--- backend/src/main/java/ru/nsu/sports/complex/backend/repository/UserRepository.java
+++ backend/src/main/java/ru/nsu/sports/complex/backend/repository/UserRepository.java
@@ -7,10 +7,7 @@ import ru.nsu.sports.complex.backend.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByName(String name);
-
     User findByEmail(String email);
-
     boolean existsByName(String name);
-
     boolean existsByEmail(String email);
 }
