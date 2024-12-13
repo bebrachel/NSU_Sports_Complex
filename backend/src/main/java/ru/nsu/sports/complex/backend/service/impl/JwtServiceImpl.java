e14bb8b082c3a14acf799aca43a50cbae9ff0e9c
Nikolay <58514394+nocarend@users.noreply.github.com>
2024-12-13 12:11:12 +0700

    wip

--- backend/src/main/java/ru/nsu/sports/complex/backend/service/impl/JwtServiceImpl.java
+++ backend/src/main/java/ru/nsu/sports/complex/backend/service/impl/JwtServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cglib.core.internal.Function;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import ru.nsu.sports.complex.backend.model.User;
 import ru.nsu.sports.complex.backend.repository.MemberRepository;
@@ -19,7 +20,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-@Service
+@Component
 public class JwtServiceImpl implements JwtService {
     @Value("${token.signing.key}")
     private String jwtSigningKey;
