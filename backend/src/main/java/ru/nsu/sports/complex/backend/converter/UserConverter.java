5d2ace00e626f480bdc88307ca96ddae1cddb068
Nikolay <58514394+nocarend@users.noreply.github.com>
2024-12-14 03:37:21 +0700

    scrum-46: implement users interop with tests

--- backend/src/main/java/ru/nsu/sports/complex/backend/converter/UserConverter.java
+++ backend/src/main/java/ru/nsu/sports/complex/backend/converter/UserConverter.java
@@ -1,4 +1,33 @@
 package ru.nsu.sports.complex.backend.converter;
 
+import ru.nsu.sports.complex.backend.dto.UserDTO;
+import ru.nsu.sports.complex.backend.model.User;
+
 public class UserConverter {
+
+    private UserConverter() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static User dtoToUser(UserDTO userDTO) {
+        if (userDTO.getName() == null || userDTO.getName().isBlank()) {
+            throw new IllegalArgumentException("Name must not be null or blank");
+        }
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setName(userDTO.getName());
+        user.setPassword(userDTO.getPassword());
+        user.setEmail(userDTO.getEmail());
+        return user;
+    }
+
+    public static UserDTO userToDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setName(user.getName());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setEmail(user.getEmail());
+        return userDTO;
+    }
+
 }
