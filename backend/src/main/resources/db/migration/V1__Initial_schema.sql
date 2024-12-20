8cd449aeb00edb9f795917144998a337dc8da193
Mikhail <misha_k_2018@mail.ru>
2024-12-20 21:02:17 +0700

    add table for connection in migration

--- backend/src/main/resources/db/migration/V1__Initial_schema.sql
+++ backend/src/main/resources/db/migration/V1__Initial_schema.sql
@@ -38,6 +38,15 @@ ALTER TABLE public.sections
         CACHE 1
         );
 
+-- Name: sections_members; Type: TABLE; Schema: public;
+CREATE TABLE public.sections_members
+(
+    section_id integer NOT NULL,
+    member_id  integer NOT NULL,
+    PRIMARY KEY (section_id, member_id),
+    CONSTRAINT fk_section FOREIGN KEY (section_id) REFERENCES public.sections (id) ON DELETE CASCADE,
+    CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES public.members (id) ON DELETE CASCADE
+);
 
 -- Name: time_slots; Type: TABLE; Schema: public;
 CREATE TABLE public.time_slots
